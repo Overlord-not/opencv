f58794c5de3b617ef1ab1cb57478ee164b303060
wang <1300070403@qq.com>
2020-07-18 17:31:55 +0800

    Second commit

--- exampl.py
+++ exampl.py
@@ -1,4 +1,8 @@
 import numpy as np
 
-n=np.array([1,2,3],ndmin=2).T
-print(n)
+n=np.array([1,2,3],ndmin=2)
+
+
+targets = np.zeros(10) + 0.01
+targets[int(all_values[0])] = 0.99
+print(targets)
--- net.py
+++ net.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 # 引用S函数
 import scipy.special as sp
+import matplotlib.pyplot as plt
 
 
 class neuralNetwork(object):
@@ -18,74 +19,98 @@ class neuralNetwork(object):
         # self.wih=np.random.normal(0.0,power(self.hiddennodes,-0.5),(self.hiddennodes,self.inputnodes))
         # self.who=np.random.normal(0.0,power(self.outputnodes,-0.5),(self.outputnodes,self.hiddennodes))
         # 神经网络抑制信号函数(具体可Google lambda函数的用法)
-        self.actions_function = lambda x:sp.expit(x)
+        self.actions_function = lambda x: sp.expit(x)
         pass
 
-
-
     # 训练神经网络
-    def train(self,inputs_list,target_list):
-      inputs = np.array(inputs_list,ndmin=2).T
-      targets = np.array(target_list,ndmin=2).T
+    def train(self, inputs_list, target_list):
+        inputs = np.array(inputs_list, ndmin=2).T
+        targets = np.array(target_list, ndmin=2).T
 
-       #输入转置
-      inputs=np.array(inputs_list,ndmin=2).T
-      # 隐藏点输入
-      hidden_inputs = np.dot(self.wih,inputs)
-      # 隐藏节点输出
-      hidden_outputs =self.actions_function(hidden_inputs)
-      #最终节点输入
-      final_inputs = np.dot(self.who,hidden_outputs)
-      #隐藏点输出
-      final_outputs = self.actions_function(final_inputs)
-      #误差计算
-      out_error = targets-final_outputs
-      #隐藏节点误差输出（误差的反向传播）
-      hidden_error = np.dot(self.who.T,out_error)
+        # 输入转置
+        inputs = np.array(inputs_list, ndmin=2).T
+        # 隐藏点输入
+        hidden_inputs = np.dot(self.wih, inputs)
+        # 隐藏节点输出
+        hidden_outputs = self.actions_function(hidden_inputs)
+        # 最终节点输入
+        final_inputs = np.dot(self.who, hidden_outputs)
+        # 隐藏点输出
+        final_outputs = self.actions_function(final_inputs)
+        # 误差计算
+        out_error = targets-final_outputs
+        # 隐藏节点误差输出（误差的反向传播）
+        hidden_error = np.dot(self.who.T, out_error)
 
-      #更新隐藏节点与输出节点的权重矩阵
-      self.who += self.learningrate*np.dot((out_error*final_outputs*(1-final_outputs)),inputs.T)
-      #更新输出节点与隐藏节点的权重矩阵
-      self.wih += self.learningrate*np.dot((hidden_error*hidden_outputs*(1-hidden_outputs)),hidden_outputs.T)
+        # 更新隐藏节点与输出节点的权重矩阵
+        self.who += self.learningrate * \
+            np.dot(out_error*final_outputs*(1-final_outputs), hidden_inputs.T)
+        # 更新输出节点与隐藏节点的权重矩阵
+        self.wih += self.learningrate * \
+            np.dot(hidden_error*hidden_outputs*(1-hidden_outputs), inputs.T)
 
-
-
-      pass
-    
-
-
+        pass
 
     # 给定输入，输出
-    def query(self,inputs_list):
-      #输入转置
-      inputs=np.array(inputs_list,ndmin=2).T
-      # 隐藏点输入
-      hidden_inputs = np.dot(self.wih,inputs)
-      # 隐藏节点输出
-      hidden_outputs =self.actions_function(hidden_inputs)
-      #最终节点输入
-      final_inputs = np.dot(self.who,hidden_outputs)
-      #隐藏点输出
-      final_outputs = self.actions_function(final_inputs)
 
-      return final_outputs
+    def query(self, inputs_list):
+        # 输入转置
+        inputs = np.array(inputs_list, ndmin=2).T
+        # 隐藏点输入
+        hidden_inputs = np.dot(self.wih, inputs)
+        # 隐藏节点输出
+        hidden_outputs = self.actions_function(hidden_inputs)
+        # 最终节点输入
+        final_inputs = np.dot(self.who, hidden_outputs)
+        # 隐藏点输出
+        final_outputs = self.actions_function(final_inputs)
+
+        return final_outputs
+
+        pass
+
+    pass
 
 
-      pass
+inputnodes = 784
+outputnodes = 10
+hiddennodes = 100
+learningrate = 0.3
+# 创建神经网络
+n = neuralNetwork(inputnodes, outputnodes, hiddennodes, learningrate)
 
+# 读取训练集
+data_file = open('mnist_train_100.csv', 'r')
+data_list = data_file.readlines()
+data_file.close()
 
+for i in range(len(data_list)):
+    # 以逗号把读取的一个长字符串进行拆分一个个字符串并创建含字符串的列表
+    all_values = data_list[i-1].split(',')
+    # 忽略第一个数字进行矩阵转换（28*28），函数asfrry字符串转换为数字并创建列表
+    inputs = (np.asfarray(all_values[1:])/255*0.99+0.01)
+    # 绘制列表
+    # plt.imshow(image_array,cmap='gray',interpolation='none')
+    # plt.show()
+    # 创建目标值 10个输出
+    targets = np.zeros(outputnodes)+0.01
+    # 读取训练集的第一个标签值，这是需要的目标值
+    targets[int(all_values[0])] = 0.99
+    #训练100次
+    n.train(inputs, targets)
     pass
 
 
 
 
+data_file = open('mnist_test_10.csv', 'r')
+data_list = data_file.readlines()
+data_file.close()
 
-inputnodes = 3
-outputnodes = 3
-hiddennodes = 3
-learningrate = 0.5
-# 创建神经网络
-n=neuralNetwork(inputnodes, outputnodes, hiddennodes, learningrate)
+# 以逗号把读取的一个长字符串进行拆分一个个字符串并创建含字符串的列表
+all_values = data_list[0].split(',')
+# 忽略第一个数字进行矩阵转换（28*28），函数asfrry字符串转换为数字并创建列表
+inputs = (np.asfarray(all_values[1:])/255.0*0.99+0.01)
 
-final_outputs=n.query([1.0,0.5,-1.5])
+final_outputs = n.query(inputs)
 print(final_outputs)
